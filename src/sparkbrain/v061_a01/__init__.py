2f4422cb161700fe99f05899f9682edde57a7fff
salmonmikan <94634410+salmonmikan@users.noreply.github.com>
2026-09-05 08:00:03 +0900

    feat(a01): add isolated v061 A01 namespace

---
+++ src/sparkbrain/v061_a01/__init__.py
@@ -0,0 +1,17 @@
+from .credit_bridge import (
+    A01CausalCreditResolution,
+    A01CausalCreditStatus,
+    A01CausalSupportState,
+    A01LocalTemporalExpectation,
+    A01PriorRelation,
+    A01TransientCreditBridge,
+)
+
+__all__ = [
+    "A01CausalCreditResolution",
+    "A01CausalCreditStatus",
+    "A01CausalSupportState",
+    "A01LocalTemporalExpectation",
+    "A01PriorRelation",
+    "A01TransientCreditBridge",
+]
